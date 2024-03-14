8d047d4b37edee34a244a7ad6d2613e85d5c0d85
UP210356 <up210356@alumnos.upa.edu.mx>
2024-03-14 13:28:11 -0600

    Async

--- Cont-Clicks/app.js
+++ Cont-Clicks/app.js
@@ -2,27 +2,28 @@ let containerClicks = document.getElementById('container-clicks');
 let btnIncrement = document.querySelector('.btn-primary');
 let btnDecrement = document.querySelector('.btn-secondary');
 let btnReset = document.querySelector('.btn-reset');
+let count = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
+  containerClicks.innerText = count;
+
+  btnIncrement.addEventListener("click", function() {
+    count++;
     containerClicks.innerText = count;
+  });
 
-        btnIncrement.addEventListener("Click", function(){
-        count ++;
-        containerClicks.innerText = count;
-    });
+  btnDecrement.onclick = function() {
+    if (count > 0) {
+      count--;
+      containerClicks.innerText = count;
+    } else {
+      alert("El contador es 0");
+    }
+  };
 
-    btnDecrement.onClick = function(){
-        if (count > 0){
-            count --;
-            containerClicks.innerText = count;
-        } else{
-            alert("El contador es 0");
-        }
-    };
-
-    btnReset.addEventListener("click", () => {
-        count = 0;
-        containerClicks.innerText = count;
-    });
+  btnReset.addEventListener("click", () => {
+    count = 0;
+    containerClicks.innerText = count;
+  });
 });
 
